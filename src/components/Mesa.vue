bfd92af949746de6d023666bcd07fd67865eee44
Gus <gus.tejada@gmail.com>
2020-04-04 18:25:37 +0200

    mas comentarios

--- src/components/Mesa.vue
+++ src/components/Mesa.vue
@@ -49,6 +49,7 @@ export default {
       //console.log("ev:", evt);
       //console.log("ev:", evt);
       //console.log("ev:", evt);
+      //console.log("ev:", evt);
       this.$emit("mouseDown", this.id, evt);
     },
     mouseUp(evt) {
